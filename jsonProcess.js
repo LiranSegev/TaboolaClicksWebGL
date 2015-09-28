36bf399cd74bf1b7e32e4727293a57f6ea07ab1c
Ran Harari <runharari@gmail.com>
2015-09-28 17:41:29 +0200

    Uploading Liran's changes.

--- jsonProcess.js
+++ jsonProcess.js
@@ -1,132 +1,121 @@
-	function jsonRequest () {
+function jsonRequest(jsonFileName) {
 
-		var request = new XMLHttpRequest();
+    var request = new XMLHttpRequest();
 
-		request.onreadystatechange = function(e) {
-			if (request.readyState === 4 && request.status === 200) {
-				data = JSON.parse(request.responseText);
-				UpdateInfoTable(data);		
-			}
-		};
-		request.open('GET', 'taboolachallange-clicks.json', true);
-		request.send();
-	};
+    request.onreadystatechange = function (e) {
+        if (request.readyState === 4 && request.status === 200) {
+            data = JSON.parse(request.responseText);
+            UpdateInfoTable(data);
+        }
+    };
+    request.open('GET', jsonFileName, true);
+    request.send();
+};
 
+function UpdateInfoTable(data) {
+    document.getElementById("currentTime").innerHTML = unixTimeToDate(new Date());
+    UpdateItemByKey(data, 'title', UpdateTopItemsDomElement);
+    UpdateItemByKey(data, 'countryName', UpdateTopCountriesDomElement);
+    AddClicksToGlobe(data);
+};
 
-	function UpdateInfoTable (data) {
+function UpdateItemByKey(data, key, updateFunction) {
 
-		document.getElementById("currentTime").innerHTML = unixTimeToDate(data.generated);
-		UpdateItemByKey(data,'title',UpdateTopItemsDomElement);
-		UpdateItemByKey(data,'countryName',UpdateTopCountriessDomElement);
-		AddClicksToGlobe(data);
-	};
+    var items = {};
+    for (var i = 0; i < data.data.length; i++) {
+        var itemTitle = data.data[i][key];
+        if (items.hasOwnProperty(itemTitle)) {
+            items[itemTitle].value++;
+        }
+        else {
+            items[itemTitle] = {
+                'value': 1,
+                'img': data.data[i].thumbnailUrl
+            }
+        }
+    }
+    ;
+    var sortedItems = SortItemsByClicks(items);
+    updateFunction(sortedItems);
 
-	function UpdateItemByKey (data, key, updateFunction){
+}
 
-		var items = {};
-		for (var i = 0; i < data.data.length; i++) {
-			var itemTitle = data.data[i][key];
-			if (items.hasOwnProperty(itemTitle))
-			{
-				items[itemTitle].value++;
-			}
-			else
-			{
-				items[itemTitle] = {
-					'value' : 1,
-					'img' : data.data[i].thumbnailUrl
-				}
-			}
-		};
-		var sortedItems = SortItemsByClicks(items);
-		updateFunction(sortedItems);
+function SortItemsByClicks(items, key) {
+    var sortedItems = [];
+    for (var item in items) {
+        sortedItems.push({
+            'key': item,
+            'value': items[item].value,
+            'img': items[item].img
+        });
+    }
+    sortedItems.sort(function (a, b) {
+        return b.value - a.value;
+    });
 
-	}
+    return sortedItems;
+}
 
-	function SortItemsByClicks(items, key)
-	{
-		var sortedItems = [];
-		for (var item in items) {	
-			sortedItems.push({
-				'key': item,
-				'value': items[item].value,
-				'img' : items[item].img
-			});
-		}
-		sortedItems.sort(function(a, b) { return  b.value - a.value; });
+function UpdateTopItemsDomElement(sortedItems) {
 
-		return sortedItems;
-	}
+    document.getElementById('itemClickLogo1').getElementsByTagName('img')[0].src = sortedItems[0].img;
+    document.getElementById('itemClickLogo2').getElementsByTagName('img')[0].src = sortedItems[1].img;
+    document.getElementById('itemClickLogo3').getElementsByTagName('img')[0].src = sortedItems[2].img;
 
-	function UpdateTopItemsDomElement (sortedItems) {
+    document.getElementById('itemTitle1').innerHTML = sortedItems[0].key;
+    document.getElementById('itemTitle2').innerHTML = sortedItems[1].key;
+    document.getElementById('itemTitle3').innerHTML = sortedItems[2].key;
 
-		document.getElementById('itemClickLogo1').getElementsByTagName('img')[0].src = sortedItems[123].img;
-		document.getElementById('itemClickLogo2').getElementsByTagName('img')[0].src = sortedItems[100].img;
-		document.getElementById('itemClickLogo3').getElementsByTagName('img')[0].src = sortedItems[123].img;
+    document.getElementById('itemClicks1').innerHTML = sortedItems[0].value;
+    document.getElementById('itemClicks2').innerHTML = sortedItems[1].value;
+    document.getElementById('itemClicks3').innerHTML = sortedItems[2].value;
+}
 
-		document.getElementById('itemTitle1').innerHTML = sortedItems[0].key;
-		document.getElementById('itemTitle2').innerHTML = sortedItems[1].key;
-		document.getElementById('itemTitle3').innerHTML = sortedItems[2].key;
+function UpdateTopCountriesDomElement(sortedItems) {
 
-		document.getElementById('itemClicks1').innerHTML = sortedItems[0].value;
-		document.getElementById('itemClicks2').innerHTML = sortedItems[1].value;
-		document.getElementById('itemClicks3').innerHTML = sortedItems[2].value;
-	}
+    document.getElementById("country1").innerHTML = sortedItems[0].key;
+    document.getElementById("country2").innerHTML = sortedItems[1].key;
+    document.getElementById("country3").innerHTML = sortedItems[2].key;
 
-	function  UpdateTopCountriessDomElement(sortedItems) {
+    document.getElementById("countryClicks1").innerHTML = sortedItems[0].value;
+    document.getElementById("countryClicks2").innerHTML = sortedItems[1].value;
+    document.getElementById("countryClicks3").innerHTML = sortedItems[2].value;
+}
 
-		document.getElementById("country1").innerHTML = sortedItems[0].key;
-		document.getElementById("country2").innerHTML = sortedItems[1].key;
-		document.getElementById("country3").innerHTML = sortedItems[2].key;
+function AddClicksToGlobe(data) {
 
-		document.getElementById("countryClicks1").innerHTML = sortedItems[0].value;
-		document.getElementById("countryClicks2").innerHTML = sortedItems[1].value;
-		document.getElementById("countryClicks3").innerHTML = sortedItems[2].value;
-	}
+    AddClicksToGlobe.countClicks = 0;
+    var left = 0;
+    var right = parseInt(data.data.length / 60);
+    var interval = right;
 
+    var intervalID = window.setInterval(function () {
+        for (var j = left; j < right && right < data.data.length; j++) {
+            globe.AddClick(data.data[j]);
+        }
+        AddClicksToGlobe.countClicks += interval;
+        document.getElementById("numberOfClicks").innerHTML = AddClicksToGlobe.countClicks;
+        if (right >= data.data.length) {
+            clearInterval(intervalID);
+        }
+        else {
+            left = right;
+            right += interval;
+        }
 
-	function AddClicksToGlobe (data) {
+    }, 1000);
 
-		AddClicksToGlobe.countClicks= 0;
-		var left = 0;
-		var right = parseInt(data.data.length/60);
-		var interval = right;
+}
 
-		var intervalID = window.setInterval(function()
-		{
-			for (var j = left; j < right && right < data.data.length;j++)
-			{
-				globe.AddClick(data.data[j]);
-			}
-			AddClicksToGlobe.countClicks+=interval;
-			document.getElementById("numberOfClicks").innerHTML = AddClicksToGlobe.countClicks;
-			if (right >= data.data.length)
-			{
-				clearInterval(intervalID);
-			}
-			else
-			{
-				left = right;
-				right += interval;
-			}
+function unixTimeToDate(date) {
+    var dateToReturn = addZero(date.getHours()) + ' : ' + addZero(date.getMinutes());
 
-		},1000);
+    return dateToReturn;
+}
 
-	}
-
-	function unixTimeToDate (unixTime)
-	{
-		var date = new Date(unixTime * 1000);
-		var dateToReturn = addZero(date.getHours()) + ' : '+ addZero(date.getMinutes());   
-
-		return dateToReturn;  
-	}
-
-	function addZero (time) 
-	{
-		if (time < 10)
-		{  
-			time = "0" + time;
-		}
-		return time;
-	}
+function addZero(time) {
+    if (time < 10) {
+        time = "0" + time;
+    }
+    return time;
+}
